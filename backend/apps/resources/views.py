3ed091687a6b641dbafad63b0367131d15009567
Saul Enriquez <saul.enriquez2005@gmail.com>
2026-05-01 15:03:07 -0500

    fix(Security): restrict resource ViewSets to ReadOnly

--- backend/apps/resources/views.py
+++ backend/apps/resources/views.py
@@ -3,17 +3,17 @@ from rest_framework import viewsets
 from .models import Venue, AvItem, MealItem
 from .serializers import VenueSerializer, AvItemSerializer, MealItemSerializer
 
-class VenueViewSet(viewsets.ModelViewSet):
+class VenueViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Venue.objects.all()
     serializer_class = VenueSerializer
 
 
-class AvItemViewSet(viewsets.ModelViewSet):
+class AvItemViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = AvItem.objects.all()
     serializer_class = AvItemSerializer
 
 
-class MealItemViewSet(viewsets.ModelViewSet):
+class MealItemViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = MealItem.objects.all()
     serializer_class = MealItemSerializer
 
