ccf3763fa3dc25d1bea752fad6395218d4bd5ae6
Saul Enriquez <saul.enriquez2005@gmail.com>
2026-05-01 14:52:06 -0500

    fix(Security): Move CORS_ALLOW_ALL_ORIGINS to dev environment

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -24,9 +24,6 @@ sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-ptqvqeh2ob$19mx_^&502dl*9ln3@y&1#t^4372@ljljt^*6gr'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = []
 
 
@@ -126,5 +123,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CORS_ALLOW_ALL_ORIGINS = True
---
+++ backend/config/settings/prod.py
@@ -0,0 +1,5 @@
+from .base import *
+
+DEBUG = False
+
+CORS_ALLOWED_ORIGINS = []
